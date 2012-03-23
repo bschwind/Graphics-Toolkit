cc066fb4fc197504cffad9524740798b61fa8e03
Brian Schwind <brian.schwind@furman.edu>
2012-03-23 12:28:59 -0400

    Added brute force partitioning methods

---
+++ GraphicsToolkit/Physics/2D/Partitions/BruteForcePartition2D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphicsToolkit.Physics._2D.Partitions
+{
+    public class BruteForcePartition2D : Partition2D
+    {
+        public override void GenerateContacts(ref List<Bodies.RigidBody2D> bodies, ref List<Contact2D> contacts, float dt)
+        {
+            for (int i = 0; i < bodies.Count-1; i++)
+            {
+                for (int j = i + 1; j < bodies.Count; j++)
+                {
+                    contacts.Add(bodies[i].GenerateContact(bodies[j], dt));
+                }
+            }
+        }
+    }
+}
---
+++ GraphicsToolkit/Physics/3D/Partitions/BruteForcePartition3D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphicsToolkit.Physics._3D.Partitions
+{
+    public class BruteForcePartition3D : Partition3D
+    {
+        public override void GenerateContacts(ref List<Bodies.RigidBody3D> bodies, ref List<Contact3D> contacts, float dt)
+        {
+            for (int i = 0; i < bodies.Count - 1; i++)
+            {
+                for (int j = i + 1; j < bodies.Count; j++)
+                {
+                    contacts.Add(bodies[i].GenerateContact(bodies[j], dt));
+                }
+            }
+        }
+    }
+}
