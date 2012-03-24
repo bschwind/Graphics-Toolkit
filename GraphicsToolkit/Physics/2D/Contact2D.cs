a7001c0ebb9af311050a7d3edb6b3f518d38c0e5
Brian Schwind <brian.schwind@furman.edu>
2012-03-24 11:05:08 -0400

    Added normal noise to the 2D version

--- GraphicsToolkit/Physics/2D/Contact2D.cs
+++ GraphicsToolkit/Physics/2D/Contact2D.cs
@@ -15,6 +15,8 @@ namespace GraphicsToolkit.Physics._2D
         public float Impulse;
         public RigidBody2D A, B;
 
+        public static Random rand = new Random();
+
         public Contact2D(Vector2 normal, float d, RigidBody2D a, RigidBody2D b, Vector2 pointA, Vector2 pointB)
         {
             Normal = normal;
@@ -23,6 +25,8 @@ namespace GraphicsToolkit.Physics._2D
             B = b;
             this.pointA = pointA;
             this.pointB = pointB;
+
+            Normal = new Vector2((float)(Normal.X + rand.NextDouble() / 100f), (float)(Normal.Y + rand.NextDouble() / 100f));
 
             //I = (1+e)*N*(Vr • N) / (1/Ma + 1/Mb)
             //Impulse = (Vector2.Dot(b.Vel - a.Vel, normal) / (a.InvMass + b.InvMass));
