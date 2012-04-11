2cc247f724b805e2961e07a4f1fd956eb95bbd69
Brian Schwind <brian.schwind@furman.edu>
2012-04-11 12:32:48 -0400

    Started networking code

---
+++ GraphicsToolkit/Networking/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GraphicsToolkit.Networking
+{
+    public delegate void HandlePacketData(byte[] data);
+
+    public class Client
+    {
+        private TcpClient clientSocket;
+        private NetworkStream clientStream;
+        public event HandlePacketData OnDataReceived;
+
+        public Client(int port)
+        {
+            clientSocket = new TcpClient("localhost", port);
+            clientStream = clientSocket.GetStream();
+            Console.WriteLine("Connected to server");
+
+            if (OnDataReceived != null)
+            {
+                OnDataReceived(null);
+            }
+        }
+
+        public void SendData(byte[] data)
+        {
+            clientStream.Write(data, 0, data.Length);
+            clientStream.Flush();
+        }
+
+        public bool IsConnected()
+        {
+            if (OnDataReceived != null)
+            {
+                OnDataReceived(null);
+            }
+            return clientSocket.Connected;
+        }
+
+        public void Disconnect()
+        {
+            clientSocket.Close();
+        }
+    }
+}
---
+++ GraphicsToolkit/Networking/Server.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace GraphicsToolkit.Networking
+{
+    public class Server
+    {
+        TcpListener listener;
+
+        public Server(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            Console.WriteLine("Started server on port " + port);
+
+            Thread thread = new Thread(new ThreadStart(ListenForClients));
+            thread.Start();
+        }
+
+        private void ListenForClients()
+        {
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Thread clientThread = new Thread(new ParameterizedThreadStart(WorkWithClient));
+                Console.WriteLine("New client connected");
+                clientThread.Start(client);
+            }
+        }
+
+        private void WorkWithClient(object client)
+        {
+            TcpClient tcpClient = client as TcpClient;
+            if (tcpClient == null)
+            {
+                return;
+            }
+
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    //a socket error has occurred
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //the client has disconnected from the server
+                    break;
+                }
+
+                //message has successfully been received
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                Console.WriteLine(encoder.GetString(message, 0, bytesRead));
+            }
+
+            tcpClient.Close();
+        }
+    }
+}
